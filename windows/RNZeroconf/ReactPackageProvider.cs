a812ed0aa66fce2a85d31cd69b52131efa6bb98c
llaleon <lee_smit@live.nl>
2020-04-02 09:55:50 -0700

    feat(windows): windows setup

---
+++ windows/RNZeroconf/ReactPackageProvider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.ReactNative.Managed;
+
+namespace NativeModuleSample
+{
+    public sealed class ReactPackageProvider : Microsoft.ReactNative.IReactPackageProvider
+    {
+        public void CreatePackage(Microsoft.ReactNative.IReactPackageBuilder packageBuilder)
+        {
+            packageBuilder.AddAttributedModules();
+        }
+    }
+}
